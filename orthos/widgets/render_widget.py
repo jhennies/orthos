9b9ccf218a86ee71bd756a25177e8c57e865498a
jhennies <julianhennies@hotmail.de>
2015-11-27 15:51:07 +0100

    OpenGL boxes replaced by planes (depth of box = 0)

--- orthos/widgets/render_widget.py
+++ orthos/widgets/render_widget.py
@@ -47,15 +47,15 @@ class RenderWidget(QtGui.QWidget):
         shape = self.relativeInputShape
         w = self.glw
 
-        size = QtGui.QVector3D(-1*shape[0],1*shape[1],0.01)
+        size = QtGui.QVector3D(-1*shape[0],1*shape[1],0)
         self.xy = gl.GLBoxItem(size=size,color=(0,0,255))
         w.addItem(self.xy)
 
-        size = QtGui.QVector3D(-1*shape[0],0.01,-1*shape[2])
+        size = QtGui.QVector3D(-1*shape[0],0,-1*shape[2])
         self.xz = gl.GLBoxItem(size=size,color=(0,255,0))
         w.addItem(self.xz)
 
-        size = QtGui.QVector3D(0.01,1*shape[1],-1*shape[2])
+        size = QtGui.QVector3D(0,1*shape[1],-1*shape[2])
         self.yz = gl.GLBoxItem(size=size,color=(255,0,0))
         w.addItem(self.yz)
 
@@ -63,11 +63,11 @@ class RenderWidget(QtGui.QWidget):
         self.planes[1] = self.xz
         self.planes[2] = self.xy
         # self.xy.setSize
-        size = QtGui.QVector3D(-1*shape[0],-1*shape[1],0.01)
+        size = QtGui.QVector3D(-1*shape[0],-1*shape[1], 0)
         self.sections[0] = gl.GLBoxItem(size=size,color=(255,0,0))
-        size = QtGui.QVector3D(-1*shape[0],0.01,-1*shape[2])
+        size = QtGui.QVector3D(-1*shape[0],0,-1*shape[2])
         self.sections[1] = gl.GLBoxItem(size=size,color=(0,255,0))
-        size = QtGui.QVector3D(0.01,-1*shape[1],-1*shape[2])
+        size = QtGui.QVector3D(0,-1*shape[1],-1*shape[2])
         self.sections[2] = gl.GLBoxItem(size=size,color=(0,0,255))
         w.addItem(self.sections[0])
         w.addItem(self.sections[1])
@@ -106,7 +106,7 @@ class RenderWidget(QtGui.QWidget):
         minCoord1, maxCoord1 = self.widgets[1].viewBox.integralViewBounds2()
         minCoord2, maxCoord2 = self.widgets[2].viewBox.integralViewBounds2()
         if axis == 0:
-            size = QtGui.QVector3D(0.01, (maxCoord0[0]-minCoord0[0]) / min(self.inputShape), -(maxCoord0[1]-minCoord0[1]) / min(self.inputShape))
+            size = QtGui.QVector3D(0, (maxCoord0[0]-minCoord0[0]) / min(self.inputShape), -(maxCoord0[1]-minCoord0[1]) / min(self.inputShape))
             t[1] = -self.sectionPosYZ[1] + (minCoord0[0] / min(self.inputShape))
             t[2] = -self.sectionPosYZ[2] - (minCoord0[1] / min(self.inputShape))
             # print "Positions:"
@@ -118,14 +118,14 @@ class RenderWidget(QtGui.QWidget):
             self.sections[axis].translate(*tuple(t))
             self.sectionPosYZ = [t[0], (minCoord0[0] / min(self.inputShape)), -(minCoord0[1] / min(self.inputShape)) ]
         if axis == 1:
-            size = QtGui.QVector3D(-(maxCoord1[0]-minCoord1[0]) / min(self.inputShape), 0.01, -(maxCoord1[1]-minCoord1[1]) / min(self.inputShape))
+            size = QtGui.QVector3D(-(maxCoord1[0]-minCoord1[0]) / min(self.inputShape), 0, -(maxCoord1[1]-minCoord1[1]) / min(self.inputShape))
             t[0] = -self.sectionPosXZ[0] - (minCoord1[0] / min(self.inputShape))
             t[2] = -self.sectionPosXZ[2] - (minCoord1[1] / min(self.inputShape))
             self.sections[axis].setSize(size=size)
             self.sections[axis].translate(*tuple(t))
             self.sectionPosXZ = [-(minCoord1[0] / min(self.inputShape)), t[1], -(minCoord1[1] / min(self.inputShape)) ]
         if axis == 2:
-            size = QtGui.QVector3D(-(maxCoord2[0]-minCoord2[0]) / min(self.inputShape), (maxCoord2[1]-minCoord2[1]) / min(self.inputShape), 0.01)
+            size = QtGui.QVector3D(-(maxCoord2[0]-minCoord2[0]) / min(self.inputShape), (maxCoord2[1]-minCoord2[1]) / min(self.inputShape), 0)
             t[0] = -self.sectionPosXY[0] - (minCoord2[0] / min(self.inputShape))
             t[1] = -self.sectionPosXY[1] + (minCoord2[1] / min(self.inputShape))
             self.sections[axis].setSize(size=size)
